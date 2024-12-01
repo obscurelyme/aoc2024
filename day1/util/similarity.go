da3cccceeba92727600e4cc999c11adf2c8bc2c3
obscurelyme <nico.frozencoffee@gmail.com>
2024-12-01 10:44:55 -0800

    optimal runtime for part 2

--- day1/util/similarity.go
+++ day1/util/similarity.go
@@ -1,6 +1,8 @@
 package util
 
-import "github.com/obscurelyme/aoc2024/day1/types"
+import (
+	"github.com/obscurelyme/aoc2024/day1/types"
+)
 
 func incrementInMap(key int, m map[int]int) {
 	_, ok := m[key]
@@ -13,22 +15,40 @@ func incrementInMap(key int, m map[int]int) {
 	}
 }
 
+// Runtime O(n)
 func Similarity(input *types.ListInput) int {
 	occurances := map[int]int{}
 	similarity := 0
 
-	for i := 0; i < len(input.List1); i++ {
-		current := input.List1[i]
-		for j := 0; j < len(input.List2); j++ {
-			if current == input.List2[j] {
-				incrementInMap(current, occurances)
-			}
-		}
+	for i := 0; i < len(input.List2); i++ {
+		occurances[input.List2[i]]++
 	}
 
-	for key, value := range occurances {
-		similarity += key * value
+	for i := 0; i < len(input.List1); i++ {
+		current := input.List1[i]
+		similarity += occurances[current] * current
 	}
 
 	return similarity
 }
+
+// My original answer, non-optimal runtime O(n^2)
+// func Similarity(input *types.ListInput) int {
+// 	occurances := map[int]int{}
+// 	similarity := 0
+
+// 	for i := 0; i < len(input.List1); i++ {
+// 		current := input.List1[i]
+// 		for j := 0; j < len(input.List2); j++ {
+// 			if current == input.List2[j] {
+// 				incrementInMap(current, occurances)
+// 			}
+// 		}
+// 	}
+
+// 	for key, value := range occurances {
+// 		similarity += key * value
+// 	}
+
+// 	return similarity
+// }
